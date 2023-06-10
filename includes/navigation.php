fb976bdc955286752c8c25b2a82bc8b17fb5858d
Emmanuel Sofuwa <102985819+SOFSV12@users.noreply.github.com>
2023-06-10 12:19:25 +0100

    fixed href for categories anchor tag in adminnavigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -38,6 +38,7 @@
                     </li>
                     <?php 
                         if(isset($_SESSION['user_role'])){
+                            print_r($_SESSION['user_role']);
                         if(isset($_GET['p_id'])){
                             $post_id = $_GET['p_id']; 
                         echo "<li><a href='admin/posts.php?source=edit_post&p_id={$post_id}'>Edit Post</a></li>";          
